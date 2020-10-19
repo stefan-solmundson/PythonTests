44a9dc403eebbbc6619a7766be357c4872bc6496
Steffi <20004338@tafe.wa.edu.au>
2020-10-19 16:10:25 +0800

    IoT end, Applied Python start

--- ss-IoT-DataServer/app.py
+++ ss-IoT-DataServer/app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+# Python Internal Imports
+from datetime import datetime
+
+# External Imports
+from flask import Flask, render_template, jsonify
+
+# Application Imports
 from db import Database
 
 app = Flask(__name__)
 
+# Initiate the Flask Application and "Database"
+db = Database('cpu_loads.db')
+db.create()
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
@@ -14,9 +24,29 @@ def surprise():
     return "<h1>Blaaaaaa!</h1>"
 
 
-def start_up():
-    db = Database('cpu_loads.db')
-    db.create()
+# FQDN : Fully Qualified Domain Name
+#   www <-- "server", microsoft.com <-- domain
+#   .com, .net, .com.au <-- TLD
+# e.g. http://FQDN/api/cpu-load
+@app.route('/api/cpu-load')
+def api_cpu_load_default():
+    history = api_cpu_load_quantity(20)
+    return jsonify(history)
+
+
+# e.g. http://FQDN/api/cpu-load/30
+@app.route('/api/cpu-load/<quantity>')
+def api_cpu_load_quantity(quantity):
+    # TODO: check if integer if not set to 10
+    time = datetime.now()
+    data = db.get_last(quantity)
+    return data
+
+
+@app.errorhandler(404)
+# inbuilt flask method to handle errors, takes "e" (error) as a parameter
+def not_found(e):
+    render_template("404.html", error=e)
 
 
 if __name__ == "__main__":
--- ss-IoT-DataServer/db.py
+++ ss-IoT-DataServer/db.py
@@ -7,14 +7,14 @@ class Database:
         self.__table_name = table_name
 
     def __execute(self, query):
-        """
-        Convenience method that:
+        """Convenience method that:
             opens a connection,
             retrieves a cursor,
             executes a query
             and then closes the connection.
 
-        Should NOT be used for queries that retrieve data
+        This method should NOT be used for queries that retrieve data
+
         :param query:
         :return:
         """
@@ -24,10 +24,28 @@ class Database:
         connection.commit()
         connection.close()
 
-    def create(self):
+    def __retrieve(self, query):
+        """Convenience method to retrieve data from the database
+
+        :param query:
+        :return:
         """
-        Method that creates a database and table
-         if it does not exist already exist
+        connection = sqlite3.connect(self.__filename)
+        cursor = connection.cursor()
+        cursor.execute(query)
+        rows=cursor.fetchall()  # [(12,23.4), (34, 23)...]
+        count=0
+        results = {}
+        for row in rows:
+            results[count] = dict(zip(
+                [c[0] for c in cursor.description], row))
+            count += 1
+        connection.close()
+        return results
+
+    def create(self):
+        """Method that creates a database and table (if the pair do not exist already exist)
+
         :return:
         """
         sql = f"CREATE TABLE IF NOT EXISTS {self.__table_name}(" \
@@ -37,11 +55,29 @@ class Database:
         self.__execute(sql)
 
     def store(self, value):
-        """
-        Method that store a single data value into the table
+        """Method that stores a single data value into the table
+
         :param value:
         :return:
         """
         sql = f"INSERT INTO {self.__table_name} " \
               f"VALUES (null, {value}, datetime())"
         self.__execute(sql)
+
+    def get_last(self, quantity="10"):
+        """Method that return the last "quantity" results
+
+        :param quantity:
+        :return:
+        """
+        try:
+            if not quantity.isnumeric():
+                quantity = 10
+        except AttributeError:
+            value = (abs(int(quantity)))
+        else:
+            value = (abs(int(quantity)))
+        query = f"SELECT * FROM {self.__table_name} " \
+                f"ORDER BY created_at DESC " \
+                f"LIMIT {value}"
+        return self.__retrieve(query)
